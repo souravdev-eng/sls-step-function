7c34d243e6e1587f0cf0bd4d15d7f4a9902e231e
sourav-allmysons <sourav@in.allmysons.com>
2023-09-25 10:08:15 +0530

    seting checkout inventory step function and also error case

---
+++ handler.js
@@ -0,0 +1,47 @@
+'use strict';
+const aws = require('aws-sdk');
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+const documentClient = new DynamoDB.DocumentClient({ region: "ap-south-1" });
+
+const isBookAvailable = (book, quantity) => {
+  return (book.quantity - quantity) > 0;
+};
+
+
+module.exports.checkInventory = async ({ bookId, quantity }) => {
+  try {
+    let params = {
+      TableName: 'bookTabel',
+      KeyConditionExpression: 'bookId = :bookId',
+      ExpressionAttributeValues: {
+        ':bookId': bookId
+      }
+    };
+
+    const result = await documentClient.query(params).promise();
+    let book = result.Items[ 0 ];
+
+    if (isBookAvailable(book, quantity)) {
+      return book;
+    } else {
+      let bookOutOfStockError = new Error("The book is out of stock");
+      bookOutOfStockError.name = "BookOutOfStock";
+      throw bookOutOfStockError;
+    }
+
+  } catch (e) {
+    if (e.name === 'bookOutOfStock') {
+      return e;
+    } else {
+      let bookNotFoundError = new Error("Book not found");
+      bookNotFoundError.name = "BookNotFoundError";
+      throw bookNotFoundError;
+    }
+  }
+};
+
+module.exports.calculateTotal = async (event) => {
+  return "Total is calculating and Total is 120";
+};
+
+
